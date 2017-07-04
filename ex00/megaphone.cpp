4fdb6e821e44b1fc4356c3c6a96af1d0e89fe91a
Lijun Wang <wang283808065@gmail.com>
2017-07-03 17:43:28 -0700

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,4 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+class mega{
+
+public:
+	void UpLetter(char **argv);
+};
+
+void mega::UpLetter(char **argv){
+	int i = 1; 
+	int j;
+	while (argv[i]){
+		j = 0;
+		while (argv[i][j]){
+			if (argv[i][j] <= 'z' && argv[i][j] >= 'a')
+				std::cout << char(argv[i][j] - 32);
+			else
+				std::cout << argv[i][j];
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		mega megaphone;	
+		megaphone.UpLetter(argv);
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
