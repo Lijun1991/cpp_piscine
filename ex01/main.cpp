ebbbe67b67cc2cc23c7dd56e394958f4e22daedf
Lijun Wang <wang283808065@gmail.com>
2017-07-04 11:01:13 -0700

    two d array inital in class

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -10,36 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
-#include <iomanip>
 #include "phonebook.hpp"
 
 int main(void)
 {
+	contact contact;
 	char command[10];
-	const char info[12] = {"first name: ", "last name: ", 
-	"nickname: ", "login: ", "postal address: ", 
-	"email address: ", "phone number: ", "birthday date: "
-	"favorite meal: ", "underwear color: ", "darkest secret: "};
-	char input[225];
 	while (1)
 	{
-		std::cout << "Please enter a command: ";
+		std::cout << "Please enter a valid command: ";
 		cin >> command;
 		if (strcmp(command, "EXIT"))
 			return 0;
 		else if (strcmp(command, "ADD"))
 		{
-			contact contact;
 			std::cout << "Please enter new contact infomation: ";
-			for (int i = 0; i < 12, i++){
-				std::cout << info[i];
-				cin >> input;
-				contact.add(i, input);
-			}
+			contact.add(i, input);
 		}
 		else if (strcmp(command, "SEARCH"))
-			contact.search();
+		{
+			contact.search(contact.Count_contact);
+		}
 	}
 }
